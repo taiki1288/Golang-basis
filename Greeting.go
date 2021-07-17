48ee73d04035e7a8e5e895572aca39af28362277
taiki yamashita <taiki0808ty@gmail.com>
2021-07-18 08:59:22 +0900

    説明追加

--- Greeting.go
+++ Greeting.go
@@ -5,6 +5,13 @@ func main () {
 	fmt.Println("Good morning")
 
 	fmt.Println("Good afternoon")
-	
+
 	fmt.Println("Good evening")
 }
+
+// ●Goは何らかのパッケージに属している必要がある。
+// ●Goは必ずmainパッケージに属する必要がある。
+// ●fmtはフォーマットの略。
+// ●fmtパッケージをインポートすることでパッケージ内の関数を使うことができるようになる。
+// ●fmtには文字列を参照させるパッケージが入っている。
+// ●よく使う関数をまとめてパッケージと呼んでいる。
